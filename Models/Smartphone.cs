707bd86f2c8cc75fa2da1f30d9a44889b221f236
Leandro <leandrosantosconceicao@gmail.com>
2024-08-13 15:26:28 -0300

    Inicializando repositorio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia cel1 = new ("82996704242", "1100", "imei", 100);
+Iphone cel2 = new ("82996704242", "1100", "imei", 100);
+
+cel1.InstalarAplicativo("Teste 1");
+cel1.Ligar();
+cel1.ReceberLigacao();
+
+cel2.InstalarAplicativo("Teste 2");
+cel2.Ligar();
+cel2.ReceberLigacao();
